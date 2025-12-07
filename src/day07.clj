1c99fe31e1d9bc28c9d5bcfa64d1e6e861de390f
anntnzrb <anntnzrb@proton.me>
2025-12-07 16:03:54 -0500

    style: apply kibit suggestions

--- src/day07.clj
+++ src/day07.clj
@@ -1,9 +1,8 @@
 (ns day07
   "Day 7: Laboratories - Tachyon beam splitting through quantum manifold."
   (:require
-    [clojure.string :as str]
-    [clojure.test :refer [deftest is run-tests]]))
-
+   [clojure.string :as str]
+   [clojure.test :refer [deftest is run-tests]]))
 
 ;; ─────────────────────────────────────────────────────────────
 ;; Domain
@@ -38,7 +37,6 @@
 .^.^.^.^.^...^.
 ...............")
 
-
 (defn parse
   "Parse input into grid data: {:grid [[char]] :start-col int :height int :width int}"
   [input]
@@ -56,7 +54,6 @@
      :height height
      :width width}))
 
-
 ;; ─────────────────────────────────────────────────────────────
 ;; Solution
 ;; ─────────────────────────────────────────────────────────────
@@ -65,7 +62,6 @@
   "Returns true if cell is a splitter (^)."
   [c]
   (= c \^))
-
 
 (defn- simulate-with
   "Generic beam simulation. merge-fn controls how counts combine at same column."
@@ -77,29 +73,26 @@
       [splits (reduce + (vals state))]
       (let [[new-state new-splits]
             (reduce-kv
-              (fn [[acc n] col cnt]
-                (if (splitter? (get-in grid [row col]))
-                  [(cond-> acc
-                     (pos? col) (update (dec col) merge-fn cnt)
-                     (< (inc col) width) (update (inc col) merge-fn cnt))
-                   (+ n cnt)]
-                  [(update acc col merge-fn cnt) n]))
-              [{} 0]
-              state)]
+             (fn [[acc n] col cnt]
+               (if (splitter? (get-in grid [row col]))
+                 [(cond-> acc
+                    (pos? col) (update (dec col) merge-fn cnt)
+                    (< (inc col) width) (update (inc col) merge-fn cnt))
+                  (+ n cnt)]
+                 [(update acc col merge-fn cnt) n]))
+             [{} 0]
+             state)]
         (recur (inc row) new-state (+ splits new-splits))))))
-
 
 (defn part1
   "Count total number of splits in the manifold."
   [input]
   (first (simulate-with (parse input) (constantly 1))))
 
-
 (defn part2
   "Count total timelines using many-worlds interpretation."
   [input]
   (second (simulate-with (parse input) (fnil + 0))))
-
 
 ;; ─────────────────────────────────────────────────────────────
 ;; Tests
@@ -107,15 +100,13 @@
 
 (deftest test-parse
   (let [{:keys [start-row start-col height width]} (parse example)]
-    (is (= 0 start-row))
+    (is (zero? start-row))
     (is (= 7 start-col))
     (is (= 16 height))
     (is (= 15 width))))
 
-
 (deftest test-part1
   (is (= 21 (part1 example))))
-
 
 (deftest test-part2
   (is (= 40 (part2 example))))
